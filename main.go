b53a3f6588395ea2db46d4ab11b778cd9557a17a
p1en <52214354+p1en@users.noreply.github.com>
2024-11-14 14:56:12 +0900

    Compile an integer to an exectuable that exits with the given number

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "%s: invalid number of arguments\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	fmt.Printf(".intel_syntax noprefix\n")
+	fmt.Printf(".global main\n")
+	fmt.Printf("main:\n")
+	fmt.Printf("  mov rax, %s\n", os.Args[1])
+	fmt.Printf("  ret\n")
 }
